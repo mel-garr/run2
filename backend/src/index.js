cd0e0133eef2c3a831c92740bb0ccf0d9edb5507
mohamed <mohamed@mohamed.mohamed>
2025-10-09 05:16:26 -0400

    morning update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
 require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000
 
+app.use(express.json());
 
+// app.use('/api/auth', require('./authentification'));
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
